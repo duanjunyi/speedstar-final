885ff7c2baa95cfe7476fb504f48909feacb14f8
lcy <1031660311@qq.com>
2021-09-19 14:36:16 +0800

    修改rplidar_ros 复制

--- Ros/setup.sh
+++ Ros/setup.sh
@@ -4,8 +4,8 @@ cd $basepath
 
 rm -r ~/speedstar_ws
 mkdir -p ~/speedstar_ws/src
-cp -r ./auto_driver/ ./bluetooth_bridge/ ~/speedstar_ws/src/
-chmod +x ~/speedstar_ws/src/auto_driver/src/*.py ~/speedstar_ws/src/bluetooth_bridge/src/*.py
+cp -r ./auto_driver/ ./bluetooth_bridge/ ./rplidar_ros/ ~/speedstar_ws/src/
+chmod +x ~/speedstar_ws/src/auto_driver/src/*.py ~/speedstar_ws/src/bluetooth_bridge/src/*.py ~/speedstar_ws/src/rplidar_ros/src/*.py
 
 cd ~/speedstar_ws
 catkin_make
