d9a17b8051999e0697ead17f083fbe1e7271c47c
liujin <liuyvjin@qq.com>
2021-09-20 12:42:33 +0800

    <perf>对车道窗提议结果进行检查，增强鲁棒性

--- laneDetect/main_2.py
+++ laneDetect/main_2.py
@@ -3,70 +3,38 @@
 
 import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-import time
-# ms_time  = lambda: (int(round(time.time() * 1000)))
 import cv2
 import numpy as np
+import time
+# ms_time  = lambda: (int(round(time.time() * 1000)))
 
-# 透视变换
-# 快速绕圈
-# src_points = np.array([[498., 596.], [789., 596.], [250., 720.], [1050., 720.]], dtype="float32")  # 源点
-# dst_points = np.array([[300., 100.], [980., 100.], [300., 720.], [980., 720.]], dtype="float32")  # 目标点
-src_points = np.array([[399, 640], [872, 635], [261, 710], [998, 713]], dtype="float32")
-dst_points = np.array([[376, 24], [893, 24], [366, 699], [895, 702]], dtype="float32")
-# challenge_video
-# src_points = np.array([[256, 448], [475, 438], [18, 532], [791, 525]], dtype="float32")
-# dst_points = np.array([[386, 5], [841, 4], [378, 716], [860, 718]], dtype="float32")
-
-frameWidth = 1280  # 宽
-frameHeight = 720  # 长
-Mwarp = cv2.getPerspectiveTransform(src_points, dst_points)  # 透视变换矩阵计算
-camMat = np.array([[6.678151103217834e+02, 0, 6.430528691213178e+02],
-                   [0, 7.148758960098705e+02, 3.581815819255082e+02], [0, 0, 1]])  # 相机校正矩阵
-camDistortion = np.array([[-0.056882894892153, 0.002184364631645, -0.002836821379133, 0, 0]])  # 相机失真矩阵
-
-# 视觉处理
-kerSz = (3, 3)  # 膨胀与腐蚀核大小
-grayThr = 125  # 二值化阈值
-roiXRatio = 0.4  # 统计x方向上histogram时选取的y轴坐标范围，以下方底边为起始点，比例定义终止位置
-
-# 划窗检测
-winWidth = 200  # 窗的宽度
-winNum = 20  # 窗的数目
-winThr = 8    # 单条车道线需有8个框检出车道线
-pixThr = 200  # 最小连续像素，小于该长度的被舍弃以去除噪声影响
-
-# 距离映射
-roadWidCm = 80      # 道路宽度 单位：cm
-roadWidPix = 660    # 透视变换后车道线像素数
-isShow = True       # 是否返回可视化图片
 
 class laneDetect:
+    """ 车道线检测类 """
     def __init__(self, Mwarp, camMat, camDistortion, kerSz, grayThr, frameHeight, frameWidth, roiXRatio,
                 winWidth, winNum, winThr, pixThr, roadWidCm, roadWidPix, isShow):
         self.Mwarp  = Mwarp                 # 透视变换矩阵
         self.camMat = camMat                # 相机校正矩阵
         self.camDistortion = camDistortion  # 相机失真矩阵
         self.kernal = np.ones(kerSz, np.uint8)      # 定义膨胀与腐蚀的核
-        self.gray_thr = grayThr                  # 二值化阈值
+        self.gray_thr = grayThr                     # 二值化阈值
         self.frame_h = frameHeight
         self.frame_w = frameWidth
-        self.roiXRatio = roiXRatio              # 遮罩保留的y轴坐标范围，以下方底边为起始点，比例定义终止位置
-        self.win_w = winWidth                   # 窗宽
-        self.win_h = int(self.frame_h // winNum)  # 窗高
-        self.win_n = winNum                       # 窗数
-        self.show = isShow  # 是否显示
-        self.road_w_pix = roadWidPix   # 车道宽（像素,默认700）
-        self.road_w_cm  = roadWidCm  # 道路宽度 单位：cm
-        self.cm_per_pix = roadWidCm / roadWidPix  # 车道线内部一个像素对应的真实距离 单位：cm
-        self.wins_thr = winThr               # 车道线检出需满足 检出窗数 > wins_thr
-        self.pix_thr = pixThr          # 一个窗中检出车道线需满足 非零像素 > minpix
-        # 内部变量
+        self.roiXRatio = roiXRatio                  # 遮罩保留的y轴坐标范围，以下方底边为起始点，比例定义终止位置
+        self.win_w = winWidth                       # 窗宽
+        self.win_h = int(self.frame_h // winNum)    # 窗高
+        self.win_n = winNum                         # 窗数
+        self.show = isShow                          # 是否绘图显示
+        self.road_w_pix = roadWidPix                # 车道宽（像素,默认700）
+        self.road_w_cm  = roadWidCm                 # 道路宽度 单位：cm
+        self.cm_per_pix = roadWidCm / roadWidPix    # 车道线内部一个像素对应的真实距离 单位：cm
+        self.wins_thr = winThr                      # 车道线检出需满足 检出窗数 > wins_thr
+        self.pix_thr = pixThr                        # 一个窗中检出车道线需满足 非零像素 > minpix
         self.lane_xc = np.zeros((2, self.win_n)).astype(np.int32)  # 左右车道线中心点x坐标，lane_center_x[0]为左，[1]为右
         self.lane_xc[1, :] = frameWidth-1
         self.lane_yc = np.arange(int(self.frame_h - 0.5*self.win_h), 0, -self.win_h)  # 车道线中心点 y 坐标
-        self.lane_flag = np.full((2, self.win_n), False, np.bool_)  # 左右车道线 检出标志位
-        self.lane_curve = [None, None]                             # 左右拟合曲线
+        self.lane_flag = np.full((2, self.win_n), False, np.bool_)      # 左右车道线 检出标志位
+        self.lane_curve = [None, None]                               # 左右拟合曲线
         self.bias = 0           # veh_pos - cen_pos, >0偏右，<0偏左，cm
 
     def spin(self, img):
@@ -79,7 +47,7 @@ class laneDetect:
         pixes = img_prep.nonzero()  # 所有非零像素
         for i in range(self.win_n):
             # 窗中心
-            l_win_xc, r_win_xc = self.get_win_xc(img_prep, i)       # 第i层左右车道预测窗中心 x 坐标
+            l_win_xc, r_win_xc = self.check_order(*self.porpose_win_xc(img_prep, i))      # 第i层左右车道预测窗中心 x 坐标
             win_yc = self.lane_yc[i]                                # 第i层左右车道预测窗中心 y 坐标
             # 生成窗
             l_win_pts = self.get_win(pixes, xc=l_win_xc, yc=win_yc)  # 左窗中所有像素点坐标 [n,2]
@@ -139,7 +107,6 @@ class laneDetect:
 
         #--- 绘图
         if self.show:
-
             # (1) 绘制车道点
             xc_idxs = self.lane_flag.nonzero()
             xcs = self.lane_xc[xc_idxs]
@@ -165,6 +132,7 @@ class laneDetect:
             else:
                 pts = np.array([[0, 0]]).astype(np.int32)
             cv2.polylines(img_show, [pts,], False, (0, 255, 0), 2)
+            cv2.namedWindow('img_show', cv2.WINDOW_NORMAL)
             cv2.imshow('img_show', img_show)
             cv2.waitKey(1)
 
@@ -176,7 +144,8 @@ class laneDetect:
             font = cv2.FONT_HERSHEY_SIMPLEX
             side_text = 'right' if self.bias>0 else 'left'
             center_text = "Vehicle is %.3f cm %s of center" % (abs(self.bias), side_text)
-            cv2.putText(result, center_text, (100, 100), font, 1, (20, 20, 255), 2)
+            cv2.putText(result, center_text, (50, 50), font, 1, (20, 20, 255), 2)
+            cv2.namedWindow('result', cv2.WINDOW_NORMAL)
             cv2.imshow('result', result)
             cv2.waitKey(1)
             c = cv2.waitKey(0)
@@ -209,9 +178,21 @@ class laneDetect:
                 (px >= xc-self.win_w//2) & (px < xc + self.win_w//2) )
         return np.hstack([px[idx][:, None], py[idx][:, None]])
 
-    def get_win_xc(self, img, i):
+    def check_order(self, l_xc, r_xc):
+        """ 对 porpose_win_xc 得到的窗中心进行检查，避免两窗过于接近 """
+        if (r_xc-l_xc)<50:
+            xc_mean = np.mean(self.lane_xc, axis=1)  # 窗xc的平均值
+            error = np.abs(xc_mean - [l_xc, r_xc])
+            if error[0] < error[1]:
+                r_xc = l_xc + self.road_w_pix
+            else:
+                l_xc = r_xc - self.road_w_pix
+        return l_xc, r_xc
+
+    def porpose_win_xc(self, img, i):
         """
-        获得第i个窗的x坐标，其来源有如下几个（按照优先级排序）：
+        提议车道检测窗位置
+        获得第i个层两窗的x坐标，其来源有如下几个（按照优先级排序）：
         1. 若上一帧第 i 个窗检测到车道线，沿用x坐标
         2. 若上一帧未检出，但本帧第 i-1 个窗检出车道线，沿用其x坐标
         3. 经过上述两个步骤若只得到一侧，用一侧+/-路宽推导另一侧
@@ -300,10 +281,43 @@ class laneDetect:
 
 
 if __name__ == '__main__':
+    # 透视变换
+    # 快速绕圈
+    # src_points = np.array([[498., 596.], [789., 596.], [250., 720.], [1050., 720.]], dtype="float32")  # 源点
+    # dst_points = np.array([[300., 100.], [980., 100.], [300., 720.], [980., 720.]], dtype="float32")  # 目标点
+    src_points = np.array([[399, 640], [872, 635], [261, 710], [998, 713]], dtype="float32")
+    dst_points = np.array([[376, 24], [893, 24], [366, 699], [895, 702]], dtype="float32")
+    # challenge_video
+    # src_points = np.array([[256, 448], [475, 438], [18, 532], [791, 525]], dtype="float32")
+    # dst_points = np.array([[386, 5], [841, 4], [378, 716], [860, 718]], dtype="float32")
+
+    frameWidth = 1280  # 宽
+    frameHeight = 720  # 长
+    Mwarp = cv2.getPerspectiveTransform(src_points, dst_points)  # 透视变换矩阵计算
+    camMat = np.array([[6.678151103217834e+02, 0, 6.430528691213178e+02],
+                    [0, 7.148758960098705e+02, 3.581815819255082e+02], [0, 0, 1]])  # 相机校正矩阵
+    camDistortion = np.array([[-0.056882894892153, 0.002184364631645, -0.002836821379133, 0, 0]])  # 相机失真矩阵
+
+    # 视觉处理
+    kerSz = (3, 3)  # 膨胀与腐蚀核大小
+    grayThr = 125  # 二值化阈值
+    roiXRatio = 0.4  # 统计x方向上histogram时选取的y轴坐标范围，以下方底边为起始点，比例定义终止位置
+
+    # 划窗检测
+    winWidth = 200  # 窗的宽度
+    winNum = 20  # 窗的数目
+    winThr = 8    # 单条车道线需有8个框检出车道线
+    pixThr = 200  # 最小连续像素，小于该长度的被舍弃以去除噪声影响
+
+    # 距离映射
+    roadWidCm = 80      # 道路宽度 单位：cm
+    roadWidPix = 660    # 透视变换后车道线像素数
+    isShow = True       # 是否返回可视化图片
     cap = cv2.VideoCapture(BASE_DIR + '\\video\\快速绕圈.mp4')  # 读入视频
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)  # 设置读入图像宽
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)  # 设置读入图像长
     cap.set(cv2.CAP_PROP_FPS, 20)    # 设置读入帧率
+
     lane_det = laneDetect(Mwarp, camMat, camDistortion, kerSz, grayThr, frameHeight, frameWidth, roiXRatio,
                 winWidth, winNum, winThr, pixThr, roadWidCm, roadWidPix, isShow)
     while True:
