060875a01124b6a233eab088374137f80a6525d9
liujin <liuyvjin@qq.com>
2021-09-22 00:07:38 +0800

    <perf>更新透视变换矩阵

--- laneDetect/main_2.py
+++ laneDetect/main_2.py
@@ -312,13 +312,13 @@ class laneDetect:
 if __name__ == '__main__':
     # 透视变换
     # 快速绕圈
-    # src_points = np.array([[498., 596.], [789., 596.], [250., 720.], [1050., 720.]], dtype="float32")  # 源点
-    # dst_points = np.array([[300., 100.], [980., 100.], [300., 720.], [980., 720.]], dtype="float32")  # 目标点
-    src_points = np.array([[399, 640], [872, 635], [261, 710], [998, 713]], dtype="float32")
-    dst_points = np.array([[376, 24], [893, 24], [366, 699], [895, 702]], dtype="float32")
+    # src_points = np.array([[399, 640], [872, 635], [261, 710], [998, 713]], dtype="float32")
+    # dst_points = np.array([[376, 24], [893, 24], [366, 699], [895, 702]], dtype="float32")
     # challenge_video
     # src_points = np.array([[256, 448], [475, 438], [18, 532], [791, 525]], dtype="float32")
     # dst_points = np.array([[386, 5], [841, 4], [378, 716], [860, 718]], dtype="float32")
+    src_points = np.array([[6, 716], [8, 342], [1263, 363], [1275, 717]], dtype="float32")
+    dst_points = np.array([[524, 714], [11, 345], [1271, 348], [762, 710]], dtype="float32")
 
     frameWidth = 1280  # 宽
     frameHeight = 720  # 长
@@ -328,9 +328,9 @@ if __name__ == '__main__':
     camDistortion = np.array([[-0.056882894892153, 0.002184364631645, -0.002836821379133, 0, 0]])  # 相机失真矩阵
 
     # 视觉处理
-    kerSz = (3, 3)  # 膨胀与腐蚀核大小
-    grayThr = 125  # 二值化阈值
-    roiXRatio = 0.4  # 统计x方向上histogram时选取的y轴坐标范围，以下方底边为起始点，比例定义终止位置
+    kerSz = (5, 5)  # 膨胀与腐蚀核大小
+    grayThr = 160  # 二值化阈值
+    roiXRatio = 0.6  # 统计x方向上histogram时选取的y轴坐标范围，以下方底边为起始点，比例定义终止位置
 
     # 划窗检测
     winWidth = 200  # 窗的宽度
@@ -340,9 +340,9 @@ if __name__ == '__main__':
 
     # 距离映射
     roadWidCm = 80      # 道路宽度 单位：cm
-    roadWidPix = 660    # 透视变换后车道线像素数
+    roadWidPix = 400    # 透视变换后车道线像素数
     isShow = True       # 是否返回可视化图片
-    cap = cv2.VideoCapture(BASE_DIR + '\\video\\test1.mp4')  # 读入视频
+    cap = cv2.VideoCapture(BASE_DIR + '\\video\\test2.mp4')  # 读入视频
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, frameWidth)  # 设置读入图像宽
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frameHeight)  # 设置读入图像长
     cap.set(cv2.CAP_PROP_FPS, 20)    # 设置读入帧率
