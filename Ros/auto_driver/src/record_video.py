37c8bbeca3c6831fe8ae7a393c05243ee99616c0
liujin <liuyvjin@qq.com>
2021-09-24 10:04:14 +0800

    <feat>lidar.launch同时采图像和雷达数据

--- Ros/auto_driver/src/record_video.py
+++ Ros/auto_driver/src/record_video.py
@@ -12,7 +12,8 @@ import sys
 import signal
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 SAVE_DIR = BASE_DIR + '/out_videos'
-os.mkdir(SAVE_DIR)
+if not os.path.exists(SAVE_DIR):
+    os.mkdir(SAVE_DIR)
 
 
 def out_name():
@@ -34,10 +35,10 @@ if __name__ == '__main__':
                 out_writer = cv2.VideoWriter(out_file, fourcc, 24, (width, height), True)
                 print('Start Recording')
 
-                def sigint_handler(self, signal, frame):
+                def sigint_handler(signal, frame):
                     """ SIGINT Signal handler """
                     print "Interrupt!"
-                    out_writer.release()
+                    # out_writer.release()
                     print('Save video at: %s' % out_file)
                     sys.exit(0)
                 signal.signal(signal.SIGINT, sigint_handler)
