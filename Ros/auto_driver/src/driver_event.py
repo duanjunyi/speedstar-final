b2c92dc5bf24a918b7ae312e246277f7441666d8
liujin <liuyvjin@qq.com>
2021-09-25 20:26:54 +0800

    <fix>

--- Ros/auto_driver/src/driver_event.py
+++ Ros/auto_driver/src/driver_event.py
@@ -67,13 +67,13 @@ class FollowLaneEvent(DriverEvent):
         bias = -bias
         if bias != 0: # 直道bias控制
             if bias > 100:
-                self.direction = 60
-            elif bias > 50:
-                self.direction = 55
-            elif bias < -50:
-                self.direction = 45
-            elif bias < -100:
                 self.direction = 40
+            elif bias > 50:
+                self.direction = 45
+            elif bias < -50:
+                self.direction = 55
+            elif bias < -100:
+                self.direction = 60
         else:  # 弯道档位控制
             self.direction = int( gear + 50 )
         self.driver.set_direction(self.direction)
